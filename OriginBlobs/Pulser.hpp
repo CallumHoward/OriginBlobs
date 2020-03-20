72e76ca72de83f41e95341b853b00f940088220a
CallumHoward <callumh@cse.unsw.edu.au>
2020-03-21 00:35:14 +1100

    added pulse with pwm controlled led

---
+++ OriginBlobs/Pulser.hpp
@@ -0,0 +1,67 @@
+// Pulser.hpp
+// Callum Howard, 2020
+
+#ifndef BLINKER_HPP
+#define BLINKER_HPP
+
+#include "Utils.hpp"
+
+namespace ch {
+
+class Pulser {
+public:
+    Pulser(const uint8_t led) : mLedPin{led} {
+        // Setup timer and attach timer to a led pin
+        ledcSetup(mLedChannel, mLedBaseFreq, mTimerResolution);
+        ledcAttachPin(mLedPin, mLedChannel);
+        mDutyFactor = ipow(2, mTimerResolution) / mMaxBrightness;
+    }
+
+    // Arduino like analogWrite
+    // value has to be between 0 and valueMax
+    void ledcAnalogWrite(uint8_t channel, uint32_t value) {
+        const uint32_t duty =
+                mDutyFactor * min(value, static_cast<uint32_t>(mMaxBrightness));
+
+        // write duty to LEDC
+        ledcWrite(mLedChannel, duty);
+    }
+
+    void update() {
+        //loop to blink without delay
+        unsigned long currentMillis = millis();
+
+        if (currentMillis - mPreviousMillis >= mInterval) {
+            // save the last time you blinked the LED
+            mPreviousMillis = currentMillis;
+
+            // change the brightness for next time through the loop:
+            mBrightness = mBrightness + mFadeAmount;
+
+            // reverse the direction of the fading at the ends of the fade:
+            if (mBrightness <= 0 || mBrightness >= mMaxBrightness) {
+                mFadeAmount = -mFadeAmount;
+            }
+
+            // set the LED with the ledState of the variable:
+            ledcAnalogWrite(mLedPin, mBrightness);
+        }
+    }
+
+private:
+    const uint8_t mLedPin;
+    const uint8_t mLedChannel = 0;              // specify channel from 0 to 15
+    const uint16_t mLedBaseFreq = 5000;
+    const uint8_t mTimerResolution = 13;        // from 0 to 255
+    const uint8_t mMaxBrightness = 255;
+    const unsigned long mInterval = 30;        // interval at which to blink (milliseconds)
+    uint32_t mDutyFactor;
+    bool mTriggered = false;  //TODO include this
+    unsigned long mPreviousMillis = 0;          // will store last time LED was updated
+    uint8_t mBrightness = 0;                    // ledState used to set the LED
+    uint8_t mFadeAmount = 5;
+};
+
+} // namespace ch
+
+#endif
--- OriginBlobs/SketchHandler.hpp
+++ OriginBlobs/SketchHandler.hpp
@@ -4,9 +4,13 @@
 #ifndef SKETCHHANDLER_HPP
 #define SKETCHHANDLER_HPP
 
+#define BOARD_NAME "esp32_stacey"
+
 #include "OTAHandler.hpp"
-#include "Blinker.hpp"
+#include "Pulser.hpp"
 #include "BLEScanner.hpp"
+#include "MQTTHandler.hpp"
+//#include "BMP280.hpp"
 #include "Credentials.hpp"
 
 namespace ch {
@@ -14,23 +18,30 @@ namespace ch {
 class SketchHandler {
 public:
     SketchHandler() :
-        blinker{2},
-        ota{"esp32", ssid, password, 80} {
+        pulser{2},
+        //bmp280{},
+        ota{BOARD_NAME, ssid, password, 80}
+        //mqttHandler{}
+    {
         ota.begin();
-        scanner.setup();
+        //scanner.setup(BOARD_NAME);
     }
 
     void update() {
         ota.update();
         delay(1);
-        blinker.update();
-        scanner.update();
+        pulser.update();
+        //bmp280.update();
+        //scanner.update();
+        //mqttHandler.update();
     }
 
 private:
-    ch::Blinker blinker;
+    ch::Pulser pulser;
+    //ch::BMP280 bmp280;
     ch::OTAHandler ota;
-    ch::BLEScanner scanner;
+    //ch::BLEScanner scanner;
+    //ch::MQTTHandler mqttHandler;
 };
 
 
