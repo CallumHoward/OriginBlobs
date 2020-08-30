9a31aa744383e708e2c849ec2928eca7b1def8c5
CallumHoward <callumh@cse.unsw.edu.au>
2020-08-30 12:27:23 +1000

    rearrange some files WIP

---
+++ grpc/tutorial_edge/README.md
@@ -0,0 +1,22 @@
+# gRPC Server Client communication
+
+## Usage
+
+### Server
+
+Run the go server:
+
+```sh
+cd go_server
+go run server.go
+```
+
+### Javascript Client
+
+Run envoy layer with docker, and then frontend client:
+
+```sh
+cd js_client
+npm run start:envoy
+npm start
+```
--- grpc/tutorial_edge/go_server/server.go
+++ grpc/tutorial_edge/go_server/server.go
@@ -11,7 +11,9 @@ import (
 func main() {
     lis, err := net.Listen("tcp", ":9090")
     if err != nil {
-        log.Fatalf("Failed to listen on port 9000: %v", err)
+        log.Fatalf("Failed to listen on port 9090: %v", err)
+    } else {
+        log.Print("Listening for gRPC on port 9090")
     }
 
     s := chat.Server{}
@@ -21,6 +23,9 @@ func main() {
     chat.RegisterChatServiceServer(grpcServer, &s)
 
     if err := grpcServer.Serve(lis); err != nil {
-        log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+        log.Fatalf("Failed to serve gRPC server over port 9090: %v", err)
+    } else {
+        log.Print("Serving gRPC on port 9090")
     }
+
 }
