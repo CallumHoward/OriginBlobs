bd3353cecb2295ca520eba8ae7e850b9e455f5db
CallumHoward <callumh@cse.unsw.edu.au>
2020-09-04 19:26:01 +1000

    update build settings for new arduino cli

--- OriginBlobs/upload.sh
+++ OriginBlobs/upload.sh
@@ -2,6 +2,6 @@
 
 for attempt in {1..10}; do
     echo "Attempt $attempt"
-    arduino-cli upload -p /dev/cu.SLAB_USBtoUART --fqbn esp32:esp32:esp32 -i bin/OriginBlobs.bin
+    arduino-cli upload -p /dev/cu.SLAB_USBtoUART --fqbn esp32:esp32:esp32 --input-dir bin/OriginBlobs
     if [ $? -eq 0 ]; then break; fi
 done
